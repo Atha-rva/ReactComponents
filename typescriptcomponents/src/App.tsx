4b33942e1c3442c269eb1556d8fc422709805557
Atharva <deshmukhatharva024@gmail.com>
2024-07-30 17:07:09 +0530

    Radio Button modified for a Inner Circle

--- typescriptcomponents/src/App.tsx
+++ typescriptcomponents/src/App.tsx
@@ -1,20 +1,15 @@
 import React from "react";
-import { useStyles } from "./style";
-import { ZTagPickerDropdown } from "./Components/NewTagDropdown";
-import ZTextAreaDropdown from "./Components/TextArea";
-import ZTextArea from "./Components/TextArea";
-import { ZTagPickerContainer } from "./Components/TagpickerDropdown";
-import ZSpinButton from "./Components/SpinButton";
 import { ManufacturingTagPickerContainer } from "./Components/TagPickerComponentwithoutdropdowncheckbox";
+import { RadioBtn } from "./Components/RadioButton";
 
 
 
 const App = () => {
-  const classes = useStyles();
+  // const classes = useStyles();
   return (
     <div>
-      <ManufacturingTagPickerContainer className={""} fieldName={""} options={["ksd,f,sdfn,dsfs,dfjdsfdsfk,fsjf","ldjfksdfklsdfjkdslfjklsdfjklsd",'jkshfjkdshfjkdshfjkhfjksdfjk','kfhsdjkfhjksdfhjfjksdfkjsdfjksfjsf','bsdfbfshfhhsdfhsfbfsbnmvbvbhvhvh','sdfhjkhfsjkdfhjkshfsjkd']}/>
-      {/* <ZSpinButton zlabel="Months" zmax={12} zmin={0}/> */}
+      {/* <ManufacturingTagPickerContainer className={""} fieldName={""} options={["ksd,f,sdfn,dsfs,dfjdsfdsfk,fsjf","ldjfksdfklsdfjkdslfjklsdfjklsd",'jkshfjkdshfjkdshfjkhfjksdfjk','kfhsdjkfhjksdfhjfjksdfkjsdfjksfjsf','bsdfbfshfhhsdfhsfbfsbnmvbvbhvhvh','sdfhjkhfsjkdfhjkshfsjkd']}/> */}
+      <RadioBtn Zlabel={["Yes","No"]} ZGlobalLabel="Compaines"/>
     </div>
   );
 };
